38f51257cc3b72315863af35378eec9b62cae0ff
ItaiRubach <98430428+ItaiRubach@users.noreply.github.com>
2022-01-26 10:49:49 +0200

    Add files via upload

---
+++ mapbox/style.js
@@ -0,0 +1,34 @@
+
+var styleJSON = {
+    "version": 8,
+    "name": "qgis2web export",
+    "pitch": 0,
+    "light": {
+        "intensity": 0.2
+    },
+    "sources": {
+        "Afersimplify_0": {
+            "type": "geojson",
+            "data": json_Afersimplify_0
+        }
+                    },
+    "sprite": "",
+    "glyphs": "https://glfonts.lukasmartinelli.ch/fonts/{fontstack}/{range}.pbf",
+    "layers": [
+        {
+            "id": "background",
+            "type": "background",
+            "layout": {},
+            "paint": {
+                "background-color": "#ffffff"
+            }
+        },
+        {
+            "id": "lyr_Afersimplify_0_0",
+            "type": "fill",
+            "source": "Afersimplify_0",
+            "layout": {},
+            "paint": {'fill-opacity': 1.0, 'fill-color': '#8d5a99'}
+        }
+],
+}
